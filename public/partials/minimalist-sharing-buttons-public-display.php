b9b33a6eb11bf146315e3f257b91c2fce22c6da8
Armand Philippot <git@armandphilippot.com>
2021-04-17 19:43:33 +0200

    chore: add more social networks
    Diaspora*, Mastodon, Reddit, Hacker News, Journal du Hacker

--- public/partials/minimalist-sharing-buttons-public-display.php
+++ public/partials/minimalist-sharing-buttons-public-display.php
@@ -20,9 +20,9 @@ $msbwidget_title = apply_filters( 'widget_title', $msbwidget_title, $instance, $
 $msbwidget_social_networks_list = $this->msbwidget_get_social_networks();
 $msbwidget_social_networks      = ! empty( $instance['social_networks'] ) ? $instance['social_networks'] : array();
 
-$msbwidget_post_link    = get_permalink();
-$msbwidget_post_title   = get_the_title();
-$msbwidget_post_content = get_the_excerpt();
+$msbwidget_post_link    = rawurlencode( get_permalink() );
+$msbwidget_post_title   = rawurlencode( get_the_title() );
+$msbwidget_post_content = rawurlencode( get_the_excerpt() );
 $msbwidget_post_image   = get_the_post_thumbnail();
 
 echo wp_kses_post( $args['before_widget'] );
@@ -31,10 +31,16 @@ if ( ! empty( $msbwidget_title ) ) {
 }
 foreach ( $msbwidget_social_networks_list as $msbwidget_social_network ) {
 	if ( array_key_exists( $msbwidget_social_network->id, $msbwidget_social_networks ) && $msbwidget_social_networks[ $msbwidget_social_network->id ] ) {
-		$msbwidget_social_network_class       = 'msbwidget__btn msbwidget__btn--' . $msbwidget_social_network->id;
-		$msbwidget_social_network_sharing_url = $msbwidget_social_network->sharing_url;
-		$msbwidget_social_network_parameters  = (array) $msbwidget_social_network->parameters;
-		$msbwidget_social_network_parameters  = array_filter( $msbwidget_social_network_parameters );
+		$msbwidget_social_network_class = 'msbwidget__btn msbwidget__btn--' . $msbwidget_social_network->id;
+
+		if ( 'mastodon' === $msbwidget_social_network->id ) {
+			$msbwidget_social_network_sharing_url = plugin_dir_url( __DIR__ ) . 'mastodon-sharing-button/select-instance.php';
+		} else {
+			$msbwidget_social_network_sharing_url = $msbwidget_social_network->sharing_url;
+		}
+
+		$msbwidget_social_network_parameters = (array) $msbwidget_social_network->parameters;
+		$msbwidget_social_network_parameters = array_filter( $msbwidget_social_network_parameters );
 
 		foreach ( $msbwidget_social_network_parameters as $msbwidget_parameter_name => $msbwidget_parameter_value ) {
 			if ( array_key_first( $msbwidget_social_network_parameters ) === $msbwidget_parameter_name ) {
@@ -45,16 +51,16 @@ foreach ( $msbwidget_social_networks_list as $msbwidget_social_network ) {
 
 			switch ( $msbwidget_parameter_name ) {
 				case 'url':
-					$msbwidget_social_network_sharing_url .= $msbwidget_post_link;
+					$msbwidget_social_network_sharing_url .= rawurlencode( $msbwidget_post_link );
 					break;
 				case 'title':
-					$msbwidget_social_network_sharing_url .= $msbwidget_post_title;
+					$msbwidget_social_network_sharing_url .= rawurlencode( $msbwidget_post_title );
 					break;
 				case 'content':
-					$msbwidget_social_network_sharing_url .= $msbwidget_post_content;
+					$msbwidget_social_network_sharing_url .= rawurlencode( $msbwidget_post_content );
 					break;
 				case 'image':
-					$msbwidget_social_network_sharing_url .= $msbwidget_post_image;
+					$msbwidget_social_network_sharing_url .= rawurlencode( $msbwidget_post_image );
 					break;
 				default:
 					break;
@@ -65,7 +71,14 @@ foreach ( $msbwidget_social_networks_list as $msbwidget_social_network ) {
 			}
 		}
 		?>
-		<a href="<?php echo esc_url( $msbwidget_social_network_sharing_url ); ?>" class="<?php echo esc_attr( $msbwidget_social_network_class ); ?>">
+		<a
+			href="<?php echo esc_url( $msbwidget_social_network_sharing_url ); ?>"
+			class="<?php echo esc_attr( $msbwidget_social_network_class ); ?>"
+			<?php if ( 'email' !== $msbwidget_social_network->id ) { ?>
+			target="popup"
+			onclick="window.open('<?php echo esc_url( $msbwidget_social_network_sharing_url ); ?>', 'popup', 'width=600, height=600'); return false;"
+			<?php } ?>
+		>
 			<?php
 			if ( 'email' === $msbwidget_social_network->id ) {
 				echo esc_html_e( 'Email', 'minimalist-sharing-buttons' );
